29f1a5403a591a3ed0e583eb7bfc5b3a95f37ad5
jitender1322 <jitendersarswat6@gmail.com>
2024-11-11 11:39:28 +0530

    practice

--- config/db.js
+++ config/db.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
 const uri = "mongodb+srv://jitendersarswat6:Jeet1322@cluster0.txt54.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log("Connected to MongoDB Atlas");
-})
-.catch((error) => {
-  console.error("Error connecting to MongoDB Atlas", error);
-});
+mongoose.connect(uri)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error("Error connecting to MongoDB Atlas", error));
+
 
 
