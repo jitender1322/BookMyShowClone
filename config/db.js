16c586d1c7982fec2e449cb0af4eaab41d90d89f
jitender1322 <jitendersarswat6@gmail.com>
2024-11-11 12:31:43 +0530

    practice

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://jitendersarswat6:Jeet1322@cluster0.txt54.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://jitendersarswat6:Jeet1322@cluster0.txt54.mongodb.net/BookMyShowDb?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(uri)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((error) => console.error("Error connecting to MongoDB Atlas", error));
